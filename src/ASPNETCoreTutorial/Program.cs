c5db5af589fecf9f6a61fc17039d74a3fefd330b
Robert <chaolong_chen@hotmail.com>
2016-11-24 23:41:35 +0800

    Init Project

--- src/ASPNETCoreTutorial/Program.cs
+++ src/ASPNETCoreTutorial/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 namespace ASPNETCoreTutorial
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
